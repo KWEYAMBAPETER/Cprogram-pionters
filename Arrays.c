c50f61e21df6a9c7ff0783c7609b817c8d450577
KWEYAMBAPETER <kweyamabapeter6@gmail.com>
2026-04-09 09:58:07 +0300

    Add Arrays

---
+++ Arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int numbers [6] = {1,3,4,5,6,7};
+    // char consonants[21] = {'A', 'C', 'D', 'U','T'};
+    // char vowels [] = {'a' , 'e','i ', 'o', 'u'};
+    printf("%d", numbers[6]);//returns a  number in the postion of the number in those square  braces
+    
+int i = 0;
+while(i < 6){
+    printf("%d\n", numbers[i]);
+    i++;
+}
+
+
+
+
+
+
+
+
+    return 0;
+}
